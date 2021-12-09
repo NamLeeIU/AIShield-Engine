399ea699143efc9781768994a8aaa6829668a41d
NamLeeIU <77669405+NamLeeIU@users.noreply.github.com>
2021-12-09 08:27:01 +0700

    Update run_download.sh

--- scripts/run_download.sh
+++ scripts/run_download.sh
@@ -1,4 +1,4 @@
-import gdown 
-url = 'https://drive.google.com/drive/folders/1Q-3FqIV8ilJnFPM4RXAPNfX-ZugX8u5M?usp=sharing'
-gdown.download_folder(url, quiet=True, no_cookies=True)
+export PYTHONPATH="$PYTHONPATH:$PWD"
+export CUDA_VISIBLE_DEVICES=''
+python3 download.py
 
