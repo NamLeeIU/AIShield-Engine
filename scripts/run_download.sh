86c46d009db9823c19be48ded5196ee6fed98608
NamLeeIU <77669405+NamLeeIU@users.noreply.github.com>
2021-12-21 17:39:01 +0700

    Delete scripts directory

--- scripts/run_download.sh
+++
@@ -1,4 +0,0 @@
-export PYTHONPATH="$PYTHONPATH:$PWD"
-export CUDA_VISIBLE_DEVICES=''
-python3 download.py
-
--- scripts/run_service.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH="$PYTHONPATH:$PWD"
-export CUDA_VISIBLE_DEVICES=''
-python3 serve.py
